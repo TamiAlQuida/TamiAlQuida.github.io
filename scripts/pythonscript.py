967cbe4b3eb8c4e282db45704ead8e9fb7b5b61a
TamiAlQuida <whozie@gmail.com>
2022-11-04 16:25:30 +0100

    fix

--- scripts/pythonscript.py
+++
@@ -1,9 +0,0 @@
-print("python interpreter below")
-
-print(""<py-repl id="my-repl" auto-generate="true"></py-script>"")
-
-# <py-script>
-#  print("python interpreter below")
-#</py-script> 
-
-#<py-repl id="my-repl" auto-generate="true"></py-script>
