5a3838b3a5be5368c335593377d7e8ea0dc189f8
TamiAlQuida <whozie@gmail.com>
2022-11-04 15:27:46 +0100

    Attempt at importing python script

---
+++ scripts/pythonscript.py
@@ -0,0 +1,9 @@
+print("python interpreter below")
+
+print(""<py-repl id="my-repl" auto-generate="true"></py-script>"")
+
+# <py-script>
+#  print("python interpreter below")
+#</py-script> 
+
+#<py-repl id="my-repl" auto-generate="true"></py-script>
