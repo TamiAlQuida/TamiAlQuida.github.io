5e6cbe1b83fae1f8ac3330f2e1610581712ea042
TamiAlQuida <whozie@gmail.com>
2022-12-23 22:08:09 +0100

    script for BME sensor

---
+++ scripts/bme280_sensor.py
@@ -0,0 +1,18 @@
+import bme280
+import smbus2
+from time import sleep
+
+port = 1
+address = 0x76 # Adafruit BME280 address. Other BME280s may be different
+bus = smbus2.SMBus(port)
+
+bme280.load_calibration_params(bus,address)
+
+while True:
+    bme280_data = bme280.sample(bus,address)
+    #bme280_data = bme280.uncompensated_readings()
+    humidity  = bme280_data.humidity
+    pressure  = bme280_data.pressure
+    ambient_temperature = bme280_data.temperature
+    print(f'{ambient_temperature:.4} C, {pressure:.6} hpa, {humidity:.4} %')
+    sleep(1) 
