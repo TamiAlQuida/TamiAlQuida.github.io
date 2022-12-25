b94ba97210fd35cd847715ede90dec6b4328a1d6
TamiAlQuida <whozie@gmail.com>
2022-12-25 11:21:57 +0100

    datetime added

--- scripts/bme280_sensor.py
+++ scripts/bme280_sensor.py
@@ -2,6 +2,7 @@ import bme280
 import smbus2
 from time import sleep
 import pandas as pd
+import datetime
 
 port = 1
 address = 0x76 # Adafruit BME280 address. Other BME280s may be different
@@ -11,11 +12,10 @@ bme280.load_calibration_params(bus,address)
 
 while True:
     bme280_data = bme280.sample(bus,address)
-    #bme280_data = bme280.uncompensated_readings()
     ambient_temperature = bme280_data.temperature
     pressure  = bme280_data.pressure
     humidity  = bme280_data.humidity
-    print(f'{ambient_temperature:.4} C, {pressure:.6} hpa, {humidity:.4} %')
+    print(datetime.datetime.now(), f'{ambient_temperature:.4} C, {pressure:.6} hPa, {humidity:.4} %')
     
     df = pd.DataFrame({'Temperature': [ambient_temperature], 'Pressure': [pressure], 'Humidity': [humidity]})
     df.to_csv('/home/pi/TamiAlQuida.github.io/scripts/bme280data.csv', mode='a', index=False, header=False)
