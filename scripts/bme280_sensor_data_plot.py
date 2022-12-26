a733b8d8ccdb526e35babe0a402b8334e61ec15a
TamiAlQuida <whozie@gmail.com>
2022-12-26 15:33:53 +0100

    clean up

--- scripts/bme280_sensor_data_plot.py
+++ scripts/bme280_sensor_data_plot.py
@@ -8,9 +8,7 @@ port = 1
 address = 0x76 # Adafruit BME280 address. Other BME280s may be different
 bus = smbus2.SMBus(port)
 
-bme280.load_calibration_params(bus,address)
-
-for i in range(0, 10):
+def measuring_atmosphere():
     bme280_data = bme280.sample(bus,address)
     ambient_temperature = bme280_data.temperature
     pressure  = bme280_data.pressure
@@ -20,6 +18,12 @@ for i in range(0, 10):
     
     df = pd.DataFrame({'Date': [date], 'Temperature C': [ambient_temperature], 'Pressure hPa': [pressure], 'Humidity %': [humidity]})
     df.to_csv('/home/pi/TamiAlQuida.github.io/scripts/bme280data.csv', mode='a', index=False, header=False)
+
+
+bme280.load_calibration_params(bus,address)
+
+for i in range(0, 10):
+    measuring_atmosphere()
     sleep(1)
 
 import plot_bme280_data
