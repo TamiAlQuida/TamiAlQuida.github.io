9fcf30993e8aad91ea4f98f825d7b42b91bfa780
TamiAlQuida <whozie@gmail.com>
2022-12-26 15:23:31 +0100

    new program that creates 10 samples and then import and prints

---
+++ scripts/plot_bme280_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+bme280_plot_data = pd.read_csv('/home/pi/TamiAlQuida.github.io/scripts/bme280data.csv')
+# print(bme280_plot_data),
+
+# print(bme280_plot_data['Date'])
+# print(bme280_plot_data['Date'].loc[bme280_plot_data.index[0]])
+
+# print(bme280_plot_data['Temperature C'])
+# print(bme280_plot_data['Pressure hPa'])
+# print(bme280_plot_data['Humidity %'].to_string(index=False))
+
+plt.plot(bme280_plot_data['Date'], (bme280_plot_data['Temperature C']))
+plt.ylabel('Temperature C')
+plt.xlabel('Date')
+plt.show()
+plt.plot(bme280_plot_data['Date'], (bme280_plot_data['Pressure hPa']))
+plt.ylabel('Pressure hPa')
+plt.xlabel('Date')
+plt.show()
+plt.plot(bme280_plot_data['Date'], (bme280_plot_data['Humidity %']))
+plt.ylabel('Humidity %')
+plt.xlabel('Date')
+plt.show()
