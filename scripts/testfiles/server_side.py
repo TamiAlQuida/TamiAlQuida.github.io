c6400f782d45924df91a5d75f7414bc804fbb8f0
TamiAlQuida <whozie@gmail.com>
2023-04-25 21:33:23 +0200

    test files

--- scripts/testfiles/server_side.py
+++ scripts/testfiles/server_side.py
@@ -1,12 +1,11 @@
 import asyncio
 import json
 import websockets
-from time import sleep
 
 speed = 0
 direction = 0
 
-async def handleData(websocket):
+async def handleData5000(websocket):
     global speed, direction
     async for message in websocket:
         data = json.loads(message)
@@ -25,17 +24,30 @@ async def handleData(websocket):
                     direction -= 1
                     print(f'Speed: {speed}, Direction: {direction}')
                     await websocket.send(json.dumps({'speed': speed, 'direction': direction}))
-                    sleep(0.07)
+                    await asyncio.sleep(0.07)
                 while direction < 0 and data['type'] != 'pressed':
                     direction += 1
                     print(f'Speed: {speed}, Direction: {direction}')
                     await websocket.send(json.dumps({'speed': speed, 'direction': direction}))
-                    sleep(0.07)
+                    await asyncio.sleep(0.07)
         print(f'Speed: {speed}, Direction: {direction}')
         await websocket.send(json.dumps({'speed': speed, 'direction': direction}))
 
+#async def handleData5001():
+#    global speed, direction
+#    while True:
+#        try:
+#            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#            sock.connect(('192.168.1.40', 5001))
+#            while True:
+#                sock.send(json.dumps({'speed': speed, 'direction': direction}).encode())
+#                await asyncio.sleep(0.07)
+#        except Exception as e:
+#            print(f"Error: {e}")
+#            await asyncio.sleep(1)
+
 async def main():
-    async with websockets.serve(handleData, 'localhost', 5000):
+    async with websockets.serve(handleData5000, 'localhost', 5000):
         print("Server started")
         await asyncio.Future()  # run forever
 
