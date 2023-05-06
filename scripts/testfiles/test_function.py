7c0353f440461a9eed35f685a1222ce84befc2ed
TamiAlQuida <whozie@gmail.com>
2023-05-06 14:24:40 +0200

    Clean up

--- scripts/testfiles/test_function.py
+++ scripts/testfiles/test_function.py
@@ -1,20 +1,4 @@
-import socket
-import asyncio
-import json
-from time import sleep
-
-speed = 1
-direction = 0
+0 = 50
+65535 = 90
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 5001))
-s.listen(0) # maybe makes it so only 1 can connect, not sure
-clientsocket, address = s.accept()
-print(f'Connection from {address}')
-
-while True:
-    #clientsocket.send(bytes(f"d= {direction}, s= {speed}", "utf8"))
-    clientsocket.send(json.dumps({'speed': speed, 'direction': direction}).encode())
-    print('sent')
-    sleep(3)
