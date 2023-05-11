21c988005e64635a551cbb152cca1c3d8b195b9a
TamiAlQuida <whozie@gmail.com>
2023-05-11 21:40:03 +0200

    clean up

--- scripts/raspberrypico/raspberry_pi_pico.py
+++ scripts/raspberrypico/raspberry_pi_pico.py
@@ -18,12 +18,17 @@ parameters = []
 speed = 0
 speed_translation = 0
 direction = 0
+direction_translation = 0
 
 #motor setup
 dc_motor_forward = Pin(1, Pin.OUT)#GP1=pin2
 dc_motor_backward = Pin(2, Pin.OUT)#GP2=pin4
 dc_motor_pwm = PWM(Pin(0)) #GP0=pin1, GND=pin3, VSYS(power)=pin39
 dc_motor_pwm.freq(100)
+
+#servo setup
+servo_motor_pwm = PWM(Pin(28))
+servo_motor_pwm.freq(50)
 
 #functions
 def connect_wifi():
@@ -48,10 +53,25 @@ async def handle_data_5001():
     parameters = data.split()
     print(parameters)
     speed = int(parameters[0])
+    direction = int(parameters[1])
     adjust_speed()
+    adjust_direction()
     if parameters == []:
         quit()
 
+def adjust_direction():
+    global direction_translation, direction
+    # .duty_16(#) takes values of 0 to 65535 for duty cycle of 0 to 100
+    # SG90 has 2 per cent duty cycle for 0 degrees, 12 per cent for 180.
+    # So a .duty_u16 value of c.1350 is zero degrees; 8200 is 180 degrees.
+    if direction == 50:
+        direction_translation = ((direction-10)*86)+1350
+        print('Direction is 0')
+        servo_motor_pwm.duty_u16(direction_translation)
+    else:
+        direction_translation = ((direction-10)*86)+1350
+        print(direction_translation)
+        servo_motor_pwm.duty_u16(direction_translation)
 
 def adjust_speed():
     global speed_translation, speed  
@@ -70,7 +90,7 @@ def adjust_speed():
         dc_motor_pwm.duty_u16(speed_translation)
     
     if speed == 50:
-        print('values at 0')
+        print('Speed is 0')
         dc_motor_backward.value(0)
         dc_motor_forward.value(0)
         dc_motor_pwm.duty_u16(0)
@@ -85,6 +105,7 @@ def turn_off():
     dc_motor_backward.value(0)
     dc_motor_forward.value(0)
     dc_motor_pwm.duty_u16(0)
+    servo_motor_pwm.duty_u16(0)
     print('done')
 
 #execution
