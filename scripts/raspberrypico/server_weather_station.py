4dc0b8d14339c7d1ce53bc9c13b1791072c7b5fe
tamialquida <whozie@gmail.com>
2023-08-07 15:03:57 +0200

    update

--- scripts/raspberrypico/server_weather_station.py
+++ scripts/raspberrypico/server_weather_station.py
@@ -11,8 +11,9 @@ data_dictionary_list = []
 def connect_to_pico():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('192.168.1.78', 5002))  # Linux Mint IP address and different port
+    #s.bind(('192.168.1.78', 5002))  # Linux Mint IP address and different port
     #s.bind(('192.168.1.96', 5002))  # Linux Ubuntu IP address and different port
+    s.bind(('192.168.1.41', 5002))  # New Linux Ubuntu IP address and different port
     s.listen(0)
     clientsocket, address = s.accept()
     print(f'Connection from {address}')
