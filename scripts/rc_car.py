9f5c7b55bf6943f4b877751e608b4dfac5a7e579
tamialquida <whozie@gmail.com>
2024-02-18 12:33:02 +0100

    dzfgsd

--- scripts/rc_car.py
+++ scripts/rc_car.py
@@ -1,11 +1,11 @@
-import RPi.GPIO as GPIO                     # controll outputs
-import time
-from evdev import InputDevice, categorize, ecodes                   # pip install evdev
 
-# Set GPIO numbering mode
+from evdev import InputDevice, categorize, ecodes  # pip install evdev
+"""import RPi.GPIO as GPIO                            # controll outputs
+
+# Set GPIO numbering mode ###############################
 GPIO.setmode(GPIO.BOARD)
 
-# Setting up pins
+# Setting up pins #######################################
 GPIO.setup(8,GPIO.OUT) # Set pin 11 as an output, and define as servo1 as PWM pin
 servo1 = GPIO.PWM(8,50) # pin 11 for servo1, pulse 50Hz
 servo1.start(0) # Start PWM running, with value of 0 (pulse off)
@@ -14,9 +14,9 @@ GPIO.setup(33,GPIO.OUT) # backward (L2)
 GPIO.setup(35,GPIO.OUT) # forward (R2)
 GPIO.setup(37,GPIO.OUT) # value
 dc_motor_1 = GPIO.PWM(37,100)
-dc_motor_1.start(0)
+dc_motor_1.start(0)"""
 
-#gamepad = InputDevice('/dev/input/event2')      # "cd /dev/input" then "ls -al" to see your connections
+#gamepad = InputDevice('/dev/input/event2') # "cd /dev/input" then "ls -al" to see your connections
 for i in range(0, 20):
     try:
         input1 = '/dev/input/event' + str(i)
@@ -51,8 +51,7 @@ left_joystick = [new_center, new_center]
 l2, r2 = 0, 0
 
 
-
-#update values#########################################
+# update values #######################################
 def update_left_joystick_position(event):
     global left_joystick
     if event.code == 0:                     # left joystick, x-axis (left/right)
@@ -73,30 +72,30 @@ def update_r2(value):
 #######################################################
 
 
-#actions###############################################
+# actions #############################################
 def move_servo():
     global left_joystick
     angle = left_joystick[0]
-    servo1.ChangeDutyCycle(2+(angle/18))
+    #servo1.ChangeDutyCycle(2+(angle/18))
 
 
 def dc_motor_backward():
     global l2
-    GPIO.output(35,False)
+    """GPIO.output(35,False)
     GPIO.output(33,True)
-    dc_motor_1.ChangeDutyCycle(l2)
+    dc_motor_1.ChangeDutyCycle(l2)"""
 
 
 def dc_motor_forward():
     global r2
-    GPIO.output(33,False)
+    """GPIO.output(33,False)
     GPIO.output(35,True)
-    dc_motor_1.ChangeDutyCycle(r2)
+    dc_motor_1.ChangeDutyCycle(r2)"""
 
 
 def turn_off():
-    servo1.stop()
-    GPIO.cleanup()
+    """servo1.stop()
+    GPIO.cleanup()"""
     print("Goodbye!")
 #####################################################
 
@@ -118,7 +117,7 @@ if __name__ == '__main__': # this condition makes the code below run only if thi
         if event.type == ecodes.EV_ABS and event.code in absolutes:                     # leftpad, joystick motion, or L2/R2 triggers
             action, value = absolutes[event.code], event.value
             
-            if event.code in [0]:                                              # joystick motion
+            if event.code in [0]:   # joystick motion
                 if event.code in [0]:                                                # left joystick moving
                     update_left_joystick_position(event)
 
@@ -128,13 +127,16 @@ if __name__ == '__main__': # this condition makes the code below run only if thi
                 print(f'{left_joystick}')
                 move_servo()
 
-            if event.code in [2]: # L2
+            elif event.code in [2]: # L2
                 update_l2(value)
                 dc_motor_backward()
                 
-            if event.code in [5]: # R2
+            elif event.code in [5]: # R2
                 update_r2(value)
                 dc_motor_forward()
+            
+            else:
+                pass
     
     turn_off()
     
