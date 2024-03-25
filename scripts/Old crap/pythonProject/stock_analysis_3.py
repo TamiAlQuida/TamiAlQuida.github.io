3ee77882fed8b688ca4715592eb11b769c507d3f
tamialquida <whozie@gmail.com>
2024-03-25 13:54:11 +0100

    dfgh

--- scripts/Old crap/pythonProject/stock_analysis_3.py
+++ scripts/Old crap/pythonProject/stock_analysis_3.py
@@ -2,9 +2,11 @@
 import pandas as pd
 import requests
 from io import StringIO
+import csv
 
 # ticker = input("What stock/ticker do you want to analyse?")
-tickers = ['AAPL', 'MSFT', 'AMZN', 'TSLA', 'META']
+tickers = ['AAPL', 'MSFT', 'BRK-B', 'AMZN', 'TSLA', 'META', 'NVDA', 'GOOG']
+#tickers = ['AAPL', 'MSFT', 'BRK-B','AMZN', 'TSLA', 'META', 'UNH', 'V', 'NVDA', 'JNJ', 'XOM', 'WMT', 'JPM', 'PG', 'MA', 'HD', 'LLY', 'PFE', 'CVX', 'BAC', 'KO', 'ABBV', 'COST', 'PEP', 'MRK', 'TMO', 'AVGO', 'DHR', 'VZ', 'ORCL', 'ABT', 'ADBE', 'ACN', 'DIS', 'CMCSA', 'MCD', 'CSCO', 'CRM', 'QCOM', 'NKE', 'TMUS', 'INTC', 'WFC', 'UPS', 'BMY', 'NEE', 'TXN', 'AMD', 'MS', 'PM', 'LIN', 'RTX', 'T', 'UNP', 'AMGN', 'LOW', 'SPGI', 'HON', 'CVS', 'MDT', 'INTU', 'SCHW', 'AMT', 'COP', 'IBM', 'AXP', 'GS', 'ELV', 'LMT', 'C', 'NFLX', 'BLK', 'DE', 'CAT', 'PYPL', 'SBUX', 'BA', 'EL', 'PLD', 'NOW', 'ADP', 'AMAT', 'ADI', 'ZTS', 'MDLZ', 'CI', 'CHTR', 'ISRG', 'DUK', 'MMC', 'CB', 'GOOG', 'GOOGL', 'MO', 'GILD', 'MMM', 'SYK', 'SO', 'GE', 'CCI', 'TJX', 'BKNG', 'CME', 'TGT', 'VRTX', 'USB', 'NOC', 'MU', 'BDX', 'CSX', 'MRNA']
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0'}
 
@@ -14,8 +16,8 @@ print("V = Intrinsic Value \n"
       "r = Expected earnings growth rate \n"
       "P/E = price per earnings \n")
 
-counter = 0
-summary_dict = {}
+counter = 1
+summary_dict = {'stock': [], 'actual price': [], 'intrinsic value': [], 'price evaluation': []}
 
 for i in tickers:
     
@@ -52,9 +54,17 @@ for i in tickers:
     #print(r)
     #print(v)
     
-    summary_dict[counter] = i, actual_price_previous_close, v, price_evaluation
-    
+    #summary_dict = {'stock': i, 'actual price': actual_price_previous_close, 'intrinsic value': v, 'price evaluation': price_evaluation}
+    summary_dict['stock'] += [i]
+    summary_dict['actual price'] += [actual_price_previous_close]
+    summary_dict['intrinsic value'] += [v]
+    summary_dict['price evaluation'] += [price_evaluation]
+    #print(summary_dict)
+
     print(f"{counter}" + "/" + f"{len(tickers)}")
     counter += 1
       
-print(summary_dict)
+df = pd.DataFrame.from_dict(summary_dict)
+print(df)
+
+df.to_csv('stock_values.csv', index=False)
