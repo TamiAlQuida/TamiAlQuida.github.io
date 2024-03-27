89d56f7c8ad5600bb01c8f69d417b5699fed95b6
TamiAlQuida <whozie@gmail.com>
2024-03-27 12:37:30 +0100

    random number

--- C++/classes.cpp
+++ C++/classes.cpp
@@ -11,36 +11,29 @@ class SkiChair {
 };
 
 void addingToAList () {
-    cout << "hello world" << "\n";
-    //return 0;
+    SkiChair listOfChairs[numberOfChairs];
+
+    for (int i = 0; i < numberOfChairs; i++)
+    {
+        listOfChairs[i].chairName = "chair" + to_string(i);
+        int random = rand() % 4;
+        listOfChairs[i].inhabitants = random;
+    }
+    
+    for (int i = 0; i < numberOfChairs; i++)
+    {
+        cout << listOfChairs[i].chairName << "\n";
+        cout << listOfChairs[i].inhabitants << "\n" << "\n";
+    }
 };
 
 int main() {
-    addingToAList();
     SkiChair chairNum1;
     chairNum1.chairName = "chairX";
     chairNum1.inhabitants = 6;
 
     cout << chairNum1.chairName << "\n";
     cout << chairNum1.inhabitants << "\n";
-    SkiChair listOfChairs[numberOfChairs];
-    //listOfChairs[0].chairName = "Chair0";
-    //listOfChairs[1].chairName = "Chair1";
-    //cout << listOfChairs[0].chairName << "\n";
-    //cout << listOfChairs[1].chairName << "\n";
-
-    for (int i = 0; i < numberOfChairs; i++)
-    {
-        listOfChairs[i].chairName = "chair" + to_string(i);
-    }
-    
-
-    for (int i = 0; i < numberOfChairs; i++)
-    {
-        cout << listOfChairs[i].chairName << "\n";
-    }
-
+    addingToAList();
     cout << listOfChairs[0].chairName << "\n";
-    cout << listOfChairs[1].chairName << "\n";
-    
 };
