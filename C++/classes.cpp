7e8866de3afe109b293e467f53f687286a6694e0
TamiAlQuida <whozie@gmail.com>
2024-03-27 10:52:53 +0100

    for loop

--- C++/classes.cpp
+++ C++/classes.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 using namespace std;
 
-string skiChairs[2][2] = {
-    {"8", "9"},
-    {"0", "1"}
-};
+int numberOfChairs = 20;
 
 class SkiChair {
     // A class that contains data about ski chairs
@@ -21,10 +18,29 @@ void addingToAList () {
 int main() {
     addingToAList();
     SkiChair chairNum1;
-    chairNum1.chairName = "chair1";
+    chairNum1.chairName = "chairX";
     chairNum1.inhabitants = 6;
 
     cout << chairNum1.chairName << "\n";
     cout << chairNum1.inhabitants << "\n";
-    cout << skiChairs[3][3] << "\n";
+    SkiChair listOfChairs[numberOfChairs];
+    //listOfChairs[0].chairName = "Chair0";
+    //listOfChairs[1].chairName = "Chair1";
+    //cout << listOfChairs[0].chairName << "\n";
+    //cout << listOfChairs[1].chairName << "\n";
+
+    for (int i = 0; i < numberOfChairs; i++)
+    {
+        listOfChairs[i].chairName = "chair" + to_string(i);
+    }
+    
+
+    for (int i = 0; i < numberOfChairs; i++)
+    {
+        cout << listOfChairs[i].chairName << "\n";
+    }
+
+    cout << listOfChairs[0].chairName << "\n";
+    cout << listOfChairs[1].chairName << "\n";
+    
 };
