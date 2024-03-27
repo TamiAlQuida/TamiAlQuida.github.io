4bbd3d77b593bf56d70f54721389b32b37ec37e9
TamiAlQuida <whozie@gmail.com>
2024-03-27 13:01:17 +0100

    sdcsd

--- C++/classes.cpp
+++ C++/classes.cpp
@@ -18,22 +18,12 @@ void addingToAList () {
         listOfChairs[i].chairName = "chair" + to_string(i);
         int random = rand() % 4;
         listOfChairs[i].inhabitants = random;
-    }
-    
-    for (int i = 0; i < numberOfChairs; i++)
-    {
+
         cout << listOfChairs[i].chairName << "\n";
         cout << listOfChairs[i].inhabitants << "\n" << "\n";
     }
 };
 
 int main() {
-    SkiChair chairNum1;
-    chairNum1.chairName = "chairX";
-    chairNum1.inhabitants = 6;
-
-    cout << chairNum1.chairName << "\n";
-    cout << chairNum1.inhabitants << "\n";
     addingToAList();
-    cout << listOfChairs[0].chairName << "\n";
 };
