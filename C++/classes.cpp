faeeeeb6dcfb884fb728645be5e226069a39842d
TamiAlQuida <whozie@gmail.com>
2024-03-27 19:47:56 +0100

    ljnl

--- C++/classes.cpp
+++ C++/classes.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 const int amountOfChairs = 20;
+int totalInhabitants = 0;
 
 class SkiChair {
     // A class that contains data about ski chairs
@@ -16,22 +17,32 @@ SkiChair listOfChairs[amountOfChairs]; //creates a list full of objects made fro
 
 void addingToAList () {
 
-    //loop through the list and add values to the objects + print + sleep
+    //loop through the list and empty chair at the top, add values to the objects + print + sleep
     for (int i = 0; i < amountOfChairs; i++)
-    {   //add values to object
+    {   
+        //empty the last chair
+        if (i < 19)
+        {
+            totalInhabitants -= listOfChairs[i+1].inhabitants;
+            listOfChairs[i+1].inhabitants = 0;
+        }
+
+        else if (i = 19)
+        {
+            totalInhabitants -= listOfChairs[0].inhabitants;
+            listOfChairs[0].inhabitants = 0;
+        }
+
+        //add values to object /fill first chair and name it
         listOfChairs[i].chairName = "chair" + to_string(i);
         int random = rand() % 5;
         listOfChairs[i].inhabitants = random;
         //print the values
         cout << listOfChairs[i].chairName << "\n";
-        cout << listOfChairs[i].inhabitants << "\n" << "\n";
-        for (int i = 0; amountOfChairs < amountOfChairs; i++)
-        {
-            int totalInhabitants =+ listOfChairs[i].inhabitants;
-            int avgInhabitants = totalInhabitants / amountOfChairs;
-        }
+        cout << listOfChairs[i].inhabitants << "\n";
         
-        cout << avgInhabitants << "\n";
+        totalInhabitants += listOfChairs[i].inhabitants;
+        cout << totalInhabitants<< "\n" << "\n";
 
         //sleep
         this_thread::sleep_for(chrono::milliseconds(1000));
