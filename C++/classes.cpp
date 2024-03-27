c6dda753def119f127d8002c36aae5b55810e728
TamiAlQuida <whozie@gmail.com>
2024-03-27 14:12:20 +0100

    kjjhkj

--- C++/classes.cpp
+++ C++/classes.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 using namespace std;
 
-int numberOfChairs = 20;
+const int amountOfChairs = 20;
 
 class SkiChair {
     // A class that contains data about ski chairs
@@ -10,20 +12,35 @@ class SkiChair {
         int inhabitants;
 };
 
+SkiChair listOfChairs[amountOfChairs]; //creates a list full of objects made from the class
+
 void addingToAList () {
-    SkiChair listOfChairs[numberOfChairs];
 
-    for (int i = 0; i < numberOfChairs; i++)
-    {
+    //loop through the list and add values to the objects + print + sleep
+    for (int i = 0; i < amountOfChairs; i++)
+    {   //add values to object
         listOfChairs[i].chairName = "chair" + to_string(i);
-        int random = rand() % 4;
+        int random = rand() % 5;
         listOfChairs[i].inhabitants = random;
-
+        //print the values
         cout << listOfChairs[i].chairName << "\n";
         cout << listOfChairs[i].inhabitants << "\n" << "\n";
+        for (int i = 0; amountOfChairs < amountOfChairs; i++)
+        {
+            int totalInhabitants =+ listOfChairs[i].inhabitants;
+            int avgInhabitants = totalInhabitants / amountOfChairs;
+        }
+        
+        cout << avgInhabitants << "\n";
+
+        //sleep
+        this_thread::sleep_for(chrono::milliseconds(1000));
     }
 };
 
 int main() {
-    addingToAList();
+    while (true)
+    {
+        addingToAList();
+    }
 };
