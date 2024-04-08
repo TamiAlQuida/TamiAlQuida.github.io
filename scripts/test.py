3a9ece9f2b08790ccdf742037fbbc1de80c8fc3c
TamiAlQuida <whozie@gmail.com>
2024-04-08 16:44:19 +0200

    calories

--- scripts/test.py
+++ scripts/test.py
@@ -1,26 +1,33 @@
-import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
+total_amount_of_calories = 672 + 500 
+amount_of_fat_contained_in_food = 672*0.55 + 500*0.56 #
 
-dagar = [0, 5, 6, 8, 9]
+fat_percentage = amount_of_fat_contained_in_food/total_amount_of_calories
 
-df3 = pd.DataFrame({'person':'Tom', 'danskadagar':[5, 2, 3, 7, 9], 'dagar':dagar})
+desired_fat_percentage = 0.7
 
-df1 = pd.DataFrame({'person':'Pauline', 'danskadagar':[5, 6, 1, 2, 3], 'dagar':[9, 5, 7, 1, 3]})
+#coconut_calories =
 
-df2 = df1.append(df3)
+#desired_fat_percentage = fat / calories
+#desired_fat_percentage* (1172 + coconut_calories) = 649.6 + coconut_calories
+#desired_fat_percentage*1172 + desired_fat_percentage*coconut_calories = 649.6 + coconut_calories
+#desired_fat_percentage*coconut_calories - coconut_calories 
+#coconut_calories(desired_fat_percentage-1) = 649.6 -(desired_fat_percentage*1172)
+coconut_calories_needed_to_stay_in_ketosis = (amount_of_fat_contained_in_food -(desired_fat_percentage*total_amount_of_calories))/(desired_fat_percentage-1)
 
-#print(df3)
-#print(df1)
 
-print(df2)
 
-keys = df2
-danskadagar= df2['danskadagar']
-dagar= df2['dagar']
-print(keys)
-print(danskadagar)
+print(total_amount_of_calories)
+print(amount_of_fat_contained_in_food)
+print(fat_percentage)
+print(coconut_calories_needed_to_stay_in_ketosis)
 
-plt.bar('danskadagar', danskadagar)
-plt.bar('dagar', dagar)
-plt.show()
+
+grams_of_coconut_oil = (100/900)*coconut_calories_needed_to_stay_in_ketosis
+print(grams_of_coconut_oil)
+
+dl_of_coconut_oil = grams_of_coconut_oil*1.08
+
+print(dl_of_coconut_oil)
+
+total_amount_of_calories_plus_coconut_oil_calories = total_amount_of_calories + coconut_calories_needed_to_stay_in_ketosis
+print(total_amount_of_calories_plus_coconut_oil_calories)
