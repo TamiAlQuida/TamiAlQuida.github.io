18adc64a2fe561a9ddfd223ad8bb75be233b8143
tamialquida <whozie@gmail.com>
2024-04-10 15:46:03 +0200

    update

--- C++/QTApp/calories.cpp
+++ C++/QTApp/calories.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
 using namespace std;
 
-float total_amount_of_calories;
-float amount_of_fat_contained_in_food;
-const float desired_fat_percentage = 0.7;
-float fat_percentage;
-float coconut_calories_needed_to_stay_in_ketosis;
+float totalAmountOfCalories;
+float weight;
+float gramsOfCarbohydratePer100g;
+float gramsOfProteinPer100g;
+float gramsOfFatPer100g;
+
+float amountOfCaloriesIn1gCarbohydrate = 4;
+float amountOfCaloriesIn1gProtein = 4;
+float amountOfCaloriesIn1gFat = 9;
+
+const float desiredFatPercentage = 0.7;
+
+float fatPercentage;
+float coconutCaloriesNeededToStayInKetosis;
 
 void inputData () {
     cout << "Amount of calories in meal?" << "\n";
-    cin >> total_amount_of_calories;
-    cout << "how much fat does it contain?" << "\n";
-    cin >> amount_of_fat_contained_in_food;
+    cin >> totalAmountOfCalories;
+    
+    cout << "how much does the food weigh?" << "\n";
+    cin >> weight;
+    cout << "how many grams of carbohydrates does it contain per 100g?" << "\n";
+    cin >> gramsOfCarbohydratePer100g;
+    cout << "how many grams of protein does it contain per 100g?" << "\n";
+    cin >> gramsOfProteinPer100g;
+    cout << "how many grams of fat does it contain per 100g?" << "\n";
+    cin >> gramsOfFatPer100g;
 
-    fat_percentage = amount_of_fat_contained_in_food / total_amount_of_calories;
-    coconut_calories_needed_to_stay_in_ketosis = (amount_of_fat_contained_in_food -(desired_fat_percentage*total_amount_of_calories))/(desired_fat_percentage-1);
-    cout << "fat percentage: " << fat_percentage << "\n";
-    cout << "calories needed from fat to stay in ketosis: " << coconut_calories_needed_to_stay_in_ketosis << "\n\n";
+    float amountOfCaloriesInCarbohydratesPer1g = (gramsOfCarbohydratePer100g * amountOfCaloriesIn1gCarbohydrate) / 100;
+    float amountOfCaloriesInProteinPer1g = (gramsOfProteinPer100g * amountOfCaloriesIn1gProtein) / 100;
+    float amountOfCaloriesInFatPer1g = (gramsOfFatPer100g * amountOfCaloriesIn1gFat) / 100;
+
+    float totalAmountOfCaloriesPer1g = (amountOfCaloriesInCarbohydratesPer1g + amountOfCaloriesInProteinPer1g + amountOfCaloriesInFatPer1g);
+    float totalAmountOfCaloriesInThisMeal = weight * totalAmountOfCaloriesPer1g;
+    float totalAmountofCaloriesInThisMealFromFat = amountOfCaloriesInFatPer1g * gramsOfFatPer100g / 100 * weight;
+
+    cout << "totalAmountOfCaloriesInThisMeal: " << totalAmountOfCaloriesInThisMeal << "\n";
+
+    cout << "totalAmountofCaloriesInThisMealFromFat: " << totalAmountofCaloriesInThisMealFromFat << "\n";
+
+    fatPercentage = amountOfCaloriesInFatPer1g / totalAmountOfCalories;
+    coconutCaloriesNeededToStayInKetosis = (amountOfCaloriesInFatPer1g -(desiredFatPercentage*totalAmountOfCalories))/(desiredFatPercentage-1);
+    cout << "fat percentage: " << fatPercentage << "\n";
+    cout << "calories needed from fat to stay in ketosis: " << coconutCaloriesNeededToStayInKetosis << "\n\n";
 };
 
 
