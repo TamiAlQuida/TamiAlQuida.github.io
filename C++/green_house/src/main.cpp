3cc6c3e74a2f3ea344eb12de21267d6c03a9699e
TamiAlQuida <whozie@gmail.com>
2025-01-29 13:03:50 +0100

    fdsgsdf

--- C++/green_house/src/main.cpp
+++ C++/green_house/src/main.cpp
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "hardware/gpio.h"
 
 int main() {
     stdio_init_all();
     cyw43_arch_init(); // Initialize WiFi chip for using the onboard LED
-    stdio_init_gpio(0, 0);
     
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
 
@@ -18,9 +18,11 @@ int main() {
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
         gpio_put(0, 0);
+        printf("LED off\n");
         sleep_ms(5000);
         gpio_put(0, 1);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        printf("LED on\n");
         sleep_ms(5000);
     }
 }
