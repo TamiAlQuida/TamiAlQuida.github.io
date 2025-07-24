d6a93b79edd67184577fa8e0bb9215081b0ff0c7
tamialquida <whozie@gmail.com>
2025-07-24 15:39:39 +0200

    stream

--- scripts/camera_test3.py
+++ scripts/camera_test3.py
@@ -4,13 +4,14 @@ import socket
 import time
 
 from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder
+from picamera2.encoders import H264Encoder, MJPEGEncoder
 from picamera2.outputs import FileOutput
 
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration({"size": (1280, 720)})
 picam2.configure(video_config)
 encoder = H264Encoder(1000000)
+#encoder = MJPEGEncoder(10000000)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -24,7 +25,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     encoder.output = FileOutput(stream)
     picam2.start_encoder(encoder)
     picam2.start()
-    time.sleep(20)
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
     picam2.stop()
     picam2.stop_encoder()
     conn.close()
