37c1e223aaf5d2ce6ec68af83e00f67fd230db90
tamialquida <whozie@gmail.com>
2025-08-12 22:38:05 +0200

    sdfgs

--- projects/picamera/camera_test4.py
+++ projects/picamera/camera_test4.py
@@ -21,7 +21,7 @@ PAGE = """\
 </head>
 <body>
 <h1>Picamera2 MJPEG Streaming Demo</h1>
-<img src="stream.mjpg" width="640" height="480" />
+<img src="stream.mjpg" width="640" height="360" />
 </body>
 </html>
 """
@@ -84,7 +84,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
+picam2.configure(picam2.create_video_configuration(main={"size": (1280, 720)})) #640 x 480
 output = StreamingOutput()
 picam2.start_recording(JpegEncoder(), FileOutput(output))
 
