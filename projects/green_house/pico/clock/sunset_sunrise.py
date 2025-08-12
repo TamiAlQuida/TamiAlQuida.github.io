6a69c4527fa91982eb645830cf2d727ad96551a9
TamiAlQuida <whozie@gmail.com>
2025-08-12 15:27:13 +0200

    datetime today + local timezone/sunset/sunrise

--- projects/green_house/pico/clock/sunset_sunrise.py
+++ projects/green_house/pico/clock/sunset_sunrise.py
@@ -5,21 +5,40 @@ from astral import LocationInfo
 latitude = 56.674  # Halmstad
 longitude = 12.857 # Halmstad
 city = LocationInfo("Halmstad", "Sweden", "Europe/Stockholm", latitude, longitude)
-s = sun(city.observer, date=datetime.date(2025, 5, 29))
+s = sun(city.observer, date=datetime.date.today(), tzinfo=city.timezone)
+
+sunrise = s["sunrise"]
+sunset = s["sunset"]
+
+date_str = sunrise.strftime("%Y-%m-%d")
+sunrise_str = sunrise.strftime("%H:%M:%S")
+sunset_str = sunset.strftime("%H:%M:%S")
+
 
 
 if __name__ == "__main__":
 
-    print((
+    print(
         f"Information for {city.name}/{city.region}\n"
         f"Timezone: {city.timezone}\n"
         f"Latitude: {city.latitude:.02f}; Longitude: {city.longitude:.02f}\n"
-    ))
+    )
 
-    print((
-    f'Dawn:    {s["dawn"]}\n'
-    f'Sunrise: {s["sunrise"]}\n'
-    f'Noon:    {s["noon"]}\n'
-    f'Sunset:  {s["sunset"]}\n'
-    f'Dusk:    {s["dusk"]}\n'
-))
+    #print(
+    #f'Dawn:    {s["dawn"]}\n'
+    #f'Sunrise: {s["sunrise"]}\n'
+    #f'Noon:    {s["noon"]}\n'
+    #f'Sunset:  {s["sunset"]}\n'
+    #f'Dusk:    {s["dusk"]}\n'
+    #)
+
+    print(date_str)
+    print(sunrise_str)
+    print(sunset_str)
+
+
+    with open("sunset_sunrise.txt", "w") as f:
+        f.write(
+            f"Date, Sunrise, Sunset \n"
+            f"{date_str}, {sunrise_str}, {sunset_str}\n"
+        )
